d02935a0ade85af92cb828cf0f16d1f77abfbf24
Daniel K <daniel.kmiotek@gmx.de>
2018-06-26 11:11:26 +0200

    DB
    - Leere DB
    - Erstellter SQLTreiber

--- SQLdriver.py
+++ SQLdriver.py
@@ -5,3 +5,16 @@ Fuert die Abfragen auf SQL aus
 Bearbeiter: Daniel
 """
 
+
+import sqlite3
+conn = conn = sqlite3.connect('database.db')
+c = conn.cursor()
+
+def writeToDatabase( insertString ):
+    c.execute( insertString )
+    conn.commit()
+    conn.close()
+
+def getFromDatabase( selectString ):
+    c.execute( selectString )
+    conn.close()
